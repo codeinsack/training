259ea520308bfb2d4256d9fa3af7de0318401b91
filtertish <ffqwerty3@gmail.com>
2019-05-09 20:09:27 +0300

    executed taks2 fizzBuzz function(1) fixed

--- src/andrey_mishinsky/2_fizzBuzz_function(1).js
+++ src/andrey_mishinsky/2_fizzBuzz_function(1).js
@@ -1,19 +1,16 @@
 function fizzBuzz() {
-    for (let i = 1; i < 101; i++) {
-        if ((i % 3 === 0) && (i % 5 === 0)) {
-            console.log(`FizzBuzz`);
-            continue;
-        }
-        if (i % 3 === 0) {
-            console.log(`Fizz`);
-            continue;
-        }
-        if (i % 5 === 0) {
-            console.log(`Buzz`);
-            continue;
-        }
-        console.log(i);
+  for (let i = 1; i < 101; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      console.log('FizzBuzz');
     }
+    else if (i % 3 === 0) {
+      console.log('Fizz');
+    }
+    else if (i % 5 === 0) {
+      console.log('Buzz');
+    }
+    else console.log(i);
+  }
 }
 
 fizzBuzz();
