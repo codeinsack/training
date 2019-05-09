936ee6740f03071d2e9901a689af0ceb3c8864ed
filtertish <ffqwerty3@gmail.com>
2019-05-09 19:41:51 +0300

    executed task2 fuzzBuzz function(1)

---
+++ src/andrey_mishinsky/2_fizzBuzz_function(1).js
@@ -0,0 +1,19 @@
+function fizzBuzz() {
+    for (let i = 1; i < 101; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0)) {
+            console.log(`FizzBuzz`);
+            continue;
+        }
+        if (i % 3 === 0) {
+            console.log(`Fizz`);
+            continue;
+        }
+        if (i % 5 === 0) {
+            console.log(`Buzz`);
+            continue;
+        }
+        console.log(i);
+    }
+}
+
+fizzBuzz();
